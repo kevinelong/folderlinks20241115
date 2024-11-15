200a0b6000fbe71244b987dfd07cdb1ca9458b9d
kevinelong <kevinelong@gmail.com>
2024-11-15 12:39:20 -0800

    web links

--- data.js
+++ data.js
@@ -1,1 +1,21 @@
 //todo create data
+folders = [
+    "JS",
+    "CSS",
+    "HTML"
+];
+
+links = [
+    {
+        folder: 0,
+        name: "MDN",
+        desc: "Mozilla",
+        href: "https://developer.mozilla.org/en-US/"
+    },
+    {
+        folder: 1,
+        name: "cssbattle",
+        desc: "CSS BATTLE",
+        href: "https://cssbattle.dev/"
+    },
+];
